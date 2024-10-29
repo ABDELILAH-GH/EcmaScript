50cdab6639d3984259887eb15f443e59a0f8eba8
abdelilah <abdelilahghoummach21@gmail.com>
2024-10-29 15:42:00 +0100

    the defrence between map and set also the best time for using map or set

---
+++ 9_map-collection/script.js
@@ -0,0 +1,16 @@
+/*
+map: lorsu'en a pas intererssé par keys (les index)
+set: lorsque nous avons interessé  de tarvaler par keys
+*/
+
+let map = new Map()
+map.set(1,'maroc')
+map.set(true,'algerie')
+map.set('1',{})
+map.set('1',{age:22})
+// console.log(map)
+// console.log(map.get(1))
+// console.log(map.keys)
+// console.log(map.size)
+// console.log(map.clear)
+// console.log(map.has("elgerie"))
