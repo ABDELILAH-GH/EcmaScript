7b370526e27ef5ee32f5c9944a41d3283c41d26b
abdelilah <abdelilahghoummach21@gmail.com>
2024-11-02 22:35:14 +0100

    update

--- 18_mini-project/etudiants.mjs
+++ 18_mini-project/etudiants.mjs
@@ -1,19 +1,37 @@
-import { ENDPOINT } from "./constants.mjs"
-export default class Etudiant {
-    constructor(name,age,note) {
-        this.name = name
-        this.age = age
-        this.note
-    }
-
-    afficher = function(){
-        return `${this.name},${this.age}.${this.note}`
+import { ENDPOINT } from "./constants.mjs";
+export class Etudiant {
+  constructor(first_name, last_name, date, note) {
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.date = date;
+    this.note = note;
   }
-    // all = function(){
-    //          let etudiants = fetch(ENDPOINT)
-    //          .then(response => response.json())
-    //          .then (etudiants=>console.log(etudiants))
-              
-    // }
+    
+  getAge = () => (new Date()).getFullYear() - new Date(this.date).getFullYear()
+  static allEtudiants = async function () {
+    const response = await fetch(ENDPOINT);
+    const etudiants = await response.json();
+    return etudiants;
+  };
 
+  // pour ajouter les donnes d'apres le formulaire a la base de donnes
+  addEtudiant = async function () {
+    const response = await fetch(ENDPOINT, {
+      // methode post pour ajouter
+      method: "POST",
+      // tu dis au serveur json que j'envoyais les donnes json
+      headers: {
+        "Content-Type": "application/json",
+      },
+      // ajouter au body
+      body: JSON.stringify({
+        first_name: this.first_name,
+        last_name: this.last_name,
+        date: this.date,
+        note: this.note,
+      }),
+    });
+    // console.log(response);
+    return response;
+  };
 }
--- 18_mini-project/js/script.mjs
+++ 18_mini-project/js/script.mjs
@@ -1,3 +1,59 @@
-import {Etudiant} from '../etudiants.mjs'
-let et = new Etudiant('ghoummach',19,13)
-console.log(et.afficher())
+import { Etudiant } from "../etudiants.mjs";
+
+// cette fonction pour recuperer les donnes
+const displayEtudiants = function () {
+  return Etudiant.allEtudiants().then(function (response) {
+    return response.map((data) => {
+      const { id, first_name, last_name, date, note} = data;
+      const etudiant = new Etudiant( first_name, last_name, date, note)
+      return `
+           <tr>
+                <td>${id}</td>
+                <td>${etudiant.first_name}</td>
+                <td>${etudiant.last_name}</td>
+                <td>${etudiant.getAge()}</td>
+                <td>${etudiant.note}</td>
+                <td><button class = "btn btn-danger btn-ms">supprimer</button></td>
+           </tr>
+           `;
+    });
+  });
+};
+
+//cette fonction pour afficher les donnes dans le tableau
+const renderEtudiants = function () {
+  // selectioner tbody
+  let body = document.querySelector(".liste-stagiaire");
+  // afficher les donnes dans le tableau
+  displayEtudiants().then((data) => {
+    body.innerHTML = data.join(" "); // si en utilise pas join en voir
+    // que entre les lignes de tableau dans le fichier html il'ya un espace
+  });
+};
+renderEtudiants();
+
+//cette fonction a permet d'ajouter un stagiaire
+const addEtudiants = ()=>{
+   // event.preventDefault() = onsubmit="return false"
+   const  [first_name,last_name,date,note] = document.querySelectorAll('#first_name,#last_name,#date,#note')
+   const etudiant = new Etudiant (first_name.value,last_name.value,date.value,note.value)
+   etudiant.addEtudiant()   
+};
+
+
+// cette fonction pour actualiser le tableau des etudiants
+const init = function () {
+  const refreshButton = document.querySelector("#refresh");
+  const addButton = document.querySelector("#add");
+   // pour actualiser
+  refreshButton.addEventListener("click", () => {
+     renderEtudiants()
+  });
+    // pour ajouter
+  addButton.addEventListener("click", () => {
+     addEtudiants()
+  });
+};
+init()
+
+
